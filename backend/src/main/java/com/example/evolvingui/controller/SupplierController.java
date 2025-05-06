834231882cccd55ff3cdce7f81a1954360bbe114
raas21 <ramrajs2102@gmail.com>
2025-05-06 12:24:04 +0530

    changed CORS config

--- backend/src/main/java/com/example/evolvingui/controller/SupplierController.java
+++ backend/src/main/java/com/example/evolvingui/controller/SupplierController.java
@@ -18,7 +18,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200", "https://localhost:4200"})
 @RestController
 @RequestMapping("/api/suppliers")
 public class SupplierController {
